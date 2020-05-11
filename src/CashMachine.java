67337fc2f5832287d8365f073526e28f2b011262
Maciej Mieczakowski <maciej.mieczakowski@gmail.com>
2020-05-11 20:32:27 +0200

    Part 2*, with loop

--- src/CashMachine.java
+++ src/CashMachine.java
@@ -1,20 +1,37 @@
+import java.util.Scanner;
+
 public class CashMachine {
 
     public static void main(String[] args) {
 
-        String[] personDetails = Utilities.collectPersonDetails();
-        Person person = Utilities.registerPerson(personDetails);
-        BankAccount bankAccount;
+        BankAccount[] bankAccounts = new BankAccount[3];
+        bankAccounts[0] = CreateBankAccount(Utilities.registerPerson(new String[]{"Mike", "Tyson", "60", "12313213213"}), 0);
+        bankAccounts[1] = CreateBankAccount(Utilities.registerPerson(new String[]{"Bill", "Clinton", "65", "564645654654"}), 1000);
+        bankAccounts[2] = CreateBankAccount(Utilities.registerPerson(new String[]{"Janusz", "Korwin", "76", "45654645645"}), 20000);
 
-        try {
-            bankAccount = CreateBankAccount(person, 3500);
-            bankAccount.deposit(1300);
-            showAccount(bankAccount);
-            bankAccount.withdraw(1100);
-            showAccount(bankAccount);
-        } catch (NotSUfficientMoneyException | ExceedDailyWithdrawAmountException e) {
-            e.printStackTrace();
-        }
+
+        Scanner scanner = new Scanner(System.in);
+        String accountIndex, operation, amount;
+        do {
+            try {
+                accountIndex = getMenuOption("Choose account:", 1, bankAccounts.length, "There is no such account.Correct value 1-" + bankAccounts.length);
+                operation = getMenuOption("Choose Operation [1 - deposit, 2 - withdraw]:", 1, 2, "There is no such operation. Choose 1 for deposit or 2 for withdraw");
+                if (Integer.parseInt(operation) == 2) {
+                    amount = getMenuOption("Type amount you wish to withdraw:", 0, 1000, "Wrong value. Maximum daily limit for withdrawal operation is 1000");
+                    bankAccounts[Integer.parseInt(accountIndex) - 1].withdraw(Double.parseDouble(amount));
+                } else {
+                    amount = getMenuOption("Type your deposit amount:", 0, 100000, "Wrong amount. You can deposit amount from 0 to 1 mln");
+                    bankAccounts[Integer.parseInt(accountIndex) - 1].deposit(Double.parseDouble(amount));
+
+                }
+                System.out.println(bankAccounts[Integer.parseInt(accountIndex) - 1]);
+                System.out.println("Do you wish to continue y/n:");
+            } catch (IllegalArgumentException | ExceedDailyWithdrawAmountException | NotSUfficientMoneyException e) {
+                e.printStackTrace();
+                System.out.println("Do you wish to continue y/n:");
+
+            }
+        } while ((scanner.nextLine().equalsIgnoreCase("y")));
     }
 
     private static BankAccount CreateBankAccount(Person person, double startAmount) {
@@ -24,4 +41,16 @@ public class CashMachine {
     private static void showAccount(BankAccount bankAccount) {
         System.out.println(bankAccount.toString());
     }
+
+    private static String getMenuOption(String question, int minTreshold, int maxTreshold, String errorMessage) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(question);
+        String testedInput = scanner.nextLine();
+        if (Integer.parseInt(testedInput) >= minTreshold && Integer.parseInt(testedInput) <= maxTreshold) {
+            return testedInput;
+        } else {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
 }
